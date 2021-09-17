008c06a45f94f952a91e1194d9f378f1d0ae2051
Dhiya Ulhaq Ramadhanty <58680899+dhiyaulhaqr@users.noreply.github.com>
2021-09-17 17:41:36 +0700

    menambahkan property dan method pada produk.php

--- produk.php
+++ produk.php
@@ -4,8 +4,25 @@
 // Komik
 // Game 
 class Produk {
-
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahPropery = "test";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+var_dump($produk3);
